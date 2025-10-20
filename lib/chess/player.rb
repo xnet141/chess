19e4ccee4ac7154ca8c155295d86dc9144bb98a4
Alex Erofeev <xnet141@gmail.com>
2025-10-20 18:27:04 +0300

    add the initialize_board method (pawns only)

--- lib/chess/player.rb
+++ lib/chess/player.rb
@@ -4,12 +4,34 @@ module Chess
   class Player
     def initialize
       @array = Array.new(8) {Array.new(8)}
+
+      array = @array
+      initialize_board array
     end
     include Chess::Board # ::
-
+    
     # class << self
     #   attr_accessor :array
     # end
+
+    def initialize_board array
+      [1, 6].each.with_index do |raw_y|
+        array[raw_y].map!.with_index do |item, column_x| # map
+          # p column_item
+          item = ImageWithArray.new(
+            'images/pawn.png',
+            x: column_x * GRID_SIZE + GRID_SIZE, y: raw_y * GRID_SIZE + GRID_SIZE,
+            width: 100, height: 100,
+            color: [1.0, 1.0, 0.2, 0.9],
+            rotate: 0,
+            z: 10,
+            data: [column_x, raw_y]
+          )
+        end
+      end
+
+      p array
+    end
     
     def logic event_x, event_y
       axis = axis(event_x, event_y)
@@ -17,14 +39,11 @@ module Chess
       return if axis.nil?
       x = axis[2]
       y = axis[3]
+      
       choose_square x, y
     end
 
     private 
-
-    def initialize_board
-      
-    end
 
     def choose_square x, y
       @array[x][y] = ImageWithArray.new(
--- lib/test.rb
+++ lib/test.rb
@@ -1,8 +1,8 @@
-hh = {0..199 => 0, 200..399 => 1, 400..600 => 2}
+# hh = {0..199 => 0, 200..399 => 1, 400..600 => 2}
 
-count = 105
+# count = 105
 # array = []
-hash = Hash.new
+# hash = Hash.new
 
 # array1 = (0..1000).step(100).to_a 
 # p array1
@@ -10,12 +10,29 @@ hash = Hash.new
 # array2 = Array.new(11) { |i| i*100 }
 # p array2
 
-array3 = Array.new(8) { |j|
-                            Array.new(2) { |i|
-                              i == 0 ? (j+1)*100...((j+1)+1)*100 : [(j + 97).chr, 8 - j] 
-                            }
-                          }.to_h # .to_h   0..100 => 0, 100..200 => 1, 200..300 => 2, 300..400 => 3, 400..500 => 4, 500..600 => 5, 600..700 => 6, 700..800 => 7, 800..900 => 8, 900..1000 => 9, 1000..1100 => 10}
-p array3
+# array3 = Array.new(8) { |j|
+#                             Array.new(2) { |i|
+#                               i == 0 ? (j+1)*100...((j+1)+1)*100 : [(j + 97).chr, 8 - j] 
+#                             }
+#                           }.to_h # .to_h   0..100 => 0, 100..200 => 1, 200..300 => 2, 300..400 => 3, 400..500 => 4, 500..600 => 5, 600..700 => 6, 700..800 => 7, 800..900 => 8, 900..1000 => 9, 1000..1100 => 10}
+# p array3
 
 # counters = Array.new(3) { Hash.new(0) }
 # p counters
+
+array = [[5,3,7,9,9],[2,6,3,9,7]]
+def initialize_board array
+  [0, 1].each.with_index do |raw, y|
+    p raw.class
+    # arr = array[raw].clone
+    array[raw].map!.with_index do |column_item, x| # map
+      p column_item
+      column_item = 42
+      p column_item
+    end
+  end
+end
+
+initialize_board array
+p array
+
